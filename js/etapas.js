c52ad55f2c17d59cba62b337e0268cf74b59c048
Oliveiralsdo <oliveira.lsdo@gmail.com>
2022-01-27 18:24:22 -0300

    início do projeto

---
+++ js/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 37222,
+                nome: 'Pernalonga',
+                partido: 'PNP',
+                fotos:[
+                    {url: 'pernalonga.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: 44471,
+                nome: 'Patolino',
+                partido: 'TAP',
+                fotos:[
+                    {url: 'patolino.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 55,
+                nome: 'Piu Piu',
+                partido: 'PPP',
+                vice: 'Frajola',
+                fotos:[
+                    {url: 'piupiu.png', legenda: 'Prefeito'},
+                    {url: 'frajola.png', legenda: 'Vice-Prefeito', small:true}
+                ] 
+            },
+            {
+                numero: 31,
+                nome: 'Porco',
+                partido: 'PGS',
+                vice: 'Taz',
+                fotos:[
+                    {url: 'porco.png', legenda: 'Prefeito'},
+                    {url: 'taz.png', legenda: 'Vice-Prefeito', small:true}
+                ] 
+            },
+        ]
+    }
+]
